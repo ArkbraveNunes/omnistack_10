2e7b2f4d71c3b90f7bbbfe48708b17db3f43ed03
Jean Santos <jean.santos@compasso.com.br>
2020-01-20 10:08:33 -0300

    Change mobile archives

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,173 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import  MapView, { Marker, Callout }  from "react-native-maps";
+import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import  api  from "../services/api";
+import  { connect, disconnect, subscribeToNewDevs }  from "../services/socket";
+
+function Main( { navigation } ) {
+    const [ currentRegion, setCurrentRegion ] = useState(null);
+    const [ devs, setdevs ] = useState([])
+    const [ techs, setTechs ] = useState('');
+
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+            // alert(granted)
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true
+                });
+
+                const { latitude, longitude } = coords;
+                // alert(coords)
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                })
+            }
+
+        }    
+    loadInitialPosition();
+    }, []);
+
+    useEffect(() => { subscribeToNewDevs(dev => setdevs([...devs, dev])) }, [devs])
+
+    function setupwebSocket() {
+        disconnect();
+        const { latitude, longitude } =  currentRegion;
+        
+        connect( latitude, longitude, techs );
+
+
+    }
+
+    async function loadDevs() {
+        const { latitude, longitude } = currentRegion
+
+        const response = await api.get('/search', {
+            params: {
+                latitude,
+                longitude,
+                technologies: techs,
+            }
+        });
+
+        // console.log(response.data.devs)
+
+        setdevs(response.data.devs);
+        setupwebSocket()
+    }
+    
+    function handleRegionChanged( region ) {
+        console.log(region)
+        setCurrentRegion( region )
+    }
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return (
+        <>
+            <MapView  onRegionChangeComplete= { handleRegionChanged } initialRegion = { currentRegion } style = { styles.map }>
+                {devs.map(dev =>(
+                    <Marker key = {dev._id}coordinate= {{ 
+                        latitude: dev.location.coordinates[1], 
+                        longitude: dev.location.coordinates[0] }}>
+                    <Image  style = { styles.avatar } source = {{ uri: dev.avatar_url }}/>
+                    <Callout onPress = {() => {
+                        navigation.navigate('Perfil do Github', { github_username: dev.git_user })
+                    } }>
+                        <View style = { styles.callout }>
+                            <Text styles = { styles.devName } >{dev.name}</Text>
+                            <Text styles = { styles.devBio } >{dev.bio}</Text>
+                            <Text styles = { styles.devTechs } >{dev.technology.join(', ')}</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+                ))}
+            </MapView>
+            <View style = {styles.searchForm}>
+                <TextInput style = { styles.searchInput } 
+                    placeholder ="Buscar devs por tecnologias..."
+                    placeholderTextColor = '#999'
+                    autoCapitalize = 'words'
+                    autoCorrect = {false}
+                    value = {techs}
+                    onChangeText = {text => setTechs(text)}/>
+                <TouchableOpacity onPress = { loadDevs } style = { styles.loadButton }>
+                    <MaterialIcons name = 'my-location' size = { 20 } colort = "#FFF"></MaterialIcons>
+                </TouchableOpacity>
+            </View>
+        </>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+    avatar: {
+        width: 40,
+        height: 40,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+    callout: {
+        width: 300,
+        alignItems:"center"
+    },
+    devName: {
+        fontWeight: "normal",
+        fontSize: 12,
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5
+    },
+    devTechs: {
+        marginTop: 5
+    },
+    searchForm : {
+        position: "absolute",
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: "row"
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: "#333",
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height:4
+        },
+        elevation: 2
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#8E4DFF",
+        borderRadius: 25,
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: 15,        
+    }
+})
+
+export default Main;
